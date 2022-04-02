001d157ab71a404c0fd74c937f56563adcf4471f
peterroe <peterroe@163.com>
2022-04-02 17:37:21 +0800

    feat: rimraf

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   root: './',
   // keep the same name as your github repos
-  base: 'vue-component-starter',
+  base: '/vue-component-starter/',
   mode: 'production',
   plugins: [vue()],
   build: {
