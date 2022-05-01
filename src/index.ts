4a7bc63bf6d4fe3eed9d39ac60b7665df4ad98dc
peterroe <linshuheng@bytedance.com>
2022-05-01 22:31:51 +0800

    feat: export default

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import type { App } from 'vue'
 import MyComponent from './MyComponent.vue'
 
-export function install(app: App) {
+function install(app: App) {
   app.component('MyComponent', MyComponent)
+}
+
+export default {
+  install
 }
